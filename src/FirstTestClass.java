7b776e07ba9dddcd98e02ff4d496d3c8fd77411f
Oksana <ksenia.prv@gmail.com>
2022-11-20 18:28:26 +0200

    Added test method

--- src/FirstTestClass.java
+++ src/FirstTestClass.java
@@ -1,4 +1,12 @@
 package src;
 
 public class FirstTestClass {
+    public static void main(String[] args) {
+        printSomeText();
+    }
+
+    public static void printSomeText() {
+        System.out.println("Some text is here");
+    }
+
 }
